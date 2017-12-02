557a1df9e6ccc329186ede5d172bcf3b384a5cae
Simon Struck <simonstruck70@gmail.com>
2017-12-02 15:11:43 +0100

    Added joystick control

--- scripts/main/mainloop.lua
+++ scripts/main/mainloop.lua
@@ -1,6 +1,8 @@
-
 return function(dt)
+    if love.joystick.getJoystickCount() > 0 then
+        core.run("player", scripts.systems.controls.joystick, { dt = dt })
+    end
     core.run("player", scripts.systems.controls.wasd, { dt = dt })
-    core.run("player", scripts.systems.controls.mouse, { dt = dt })
+    core.run("mouseplayer", scripts.systems.controls.mouse, { dt = dt })
     scripts.systems.collision.collision.functions.update(dt)
 end
--- scripts/systems/controls/wasd.lua
+++ scripts/systems/controls/wasd.lua
@@ -2,16 +2,20 @@ return function(entity, args)
     local dt = args.dt
 
     if love.keyboard.isDown("a") then
+        entity.mousecontrol = true
         entity.position.x = entity.position.x - math.cos(entity.position.rotation)* 60 * dt
         entity.position.y = entity.position.y - math.sin(entity.position.rotation)* 60 * dt
     elseif love.keyboard.isDown("d") then
+        entity.mousecontrol = true
         entity.position.x = entity.position.x + math.cos(entity.position.rotation)*  60 * dt
         entity.position.y = entity.position.y + math.sin(entity.position.rotation)* 60 * dt
     end
     if love.keyboard.isDown("w") then
+        entity.mousecontrol = true
         entity.position.x = entity.position.x + math.sin(entity.position.rotation)* 180 * dt
         entity.position.y = entity.position.y - math.cos(entity.position.rotation)* 180 * dt
     elseif love.keyboard.isDown("s") then
+        entity.mousecontrol = true
         entity.position.x = entity.position.x - math.sin(entity.position.rotation)* 180 * dt
         entity.position.y = entity.position.y + math.cos(entity.position.rotation)* 180 * dt
     end
