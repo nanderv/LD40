45ef7193dbf22a133d35eb169bf3b8fb0e2593a4
rhbvkleef <rhbvkleef@gmail.com>
2017-12-04 21:59:58 +0100

    FIxed oopsie

--- scripts/systems/money/money.lua
+++ scripts/systems/money/money.lua
@@ -103,10 +103,9 @@ money.end_raid = function(alive)
         ent.money.total, ent.money.pocket_treasure = ent.money.total + ent.money.pocket_treasure, 0
     else
         ent.money.pocket_treasure = 0
-    end
-
-    if ent.money.total  < ent.money.lastgiven * 1.05 then
-        money.next_turn()
+        if ent.money.total  < ent.money.lastgiven * 1.05 then
+            money.next_turn()
+        end
     end
 
     ent.last_turn_alive_or_new_day = alive
